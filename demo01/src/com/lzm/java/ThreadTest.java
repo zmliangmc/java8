cf33a52bd603436d9b00e659250fdaf8a435f64a
zmliangmc <zmliangmc@163.com>
2022-09-21 19:58:53 +0800

    first commit

---
+++ demo01/src/com/lzm/java/ThreadTest.java
@@ -0,0 +1,12 @@
+package com.lzm.java;
+
+/**
+ * @author lzm
+ * @create 2022-09-21 19:48
+ */
+public class ThreadTest {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
